54f4c9a66428148a7f2af339d1e5f0fe7684690a
eesha-barad <eeshabarad49@gmail.com>
2025-01-22 21:57:52 +0530

    Improved readibility

--- UserInput.java
+++ UserInput.java
@@ -26,7 +26,7 @@ class UserInput{
 		int size = scan.nextInt();
 
 		int[] array = new int[size];
-		System.out.println("Enter" + size + "elements: ");
+		System.out.println("Enter " + size + " elements: ");
 		for (int i=0; i<size; i++){
 			array[i] = scan.nextInt();
 		}
