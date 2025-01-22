816de92a9b47687fa0fe8dbb7fbbeef64bf55491
eesha-barad <eeshabarad49@gmail.com>
2025-01-22 20:44:21 +0530

    Created class objects

---
+++ Main.java
@@ -0,0 +1,22 @@
+/*
+Name: Eesha Barad
+PRN: 23070126161
+Class: AIML - B1
+Batch: 2023-27
+*/
+
+// Importing packages
+import java.util.Scanner;
+
+// Main class
+class Main{
+	
+	//Main method which is the entry point for all java programs
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Calculator calc = new Calculator();
+        UserInput input = new UserInput();
+}
+
+	
+	
