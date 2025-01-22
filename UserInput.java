47380d4addca759d4db0139e4dd12431285009ae
eesha-barad <eeshabarad49@gmail.com>
2025-01-22 20:40:39 +0530

    Added funcntion to input an array elements from user

--- UserInput.java
+++ UserInput.java
@@ -17,4 +17,19 @@ class UserInput{
 		return new int[]{first_number,second_number};
 		
 	}
+
+	// Function to input an array
+	int[] inputArray(){
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter size of array");
+		int size = scan.nextInt();
+
+		int[] array = new int[size];
+		System.out.println("Enter" + size + "elements: ");
+		for (int i=0; i<size; i++){
+			array[i] = scan.nextInt();
+		}
+		return array; 
+	}
 }
